ddbb8b9d1dd9f2f92b3a70b69ae46310ad76c9a3
TwizzyIndy <twizzy.indy@gmail.com>
2022-04-07 12:04:51 +0630

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const gplay = require('google-play-scraper');
+var moment = require('moment');
+
+function searchTop100OutdatedApps(searchTerm, minYear, country='us') {
+    gplay.search({
+            term: searchTerm,
+            num: 250,
+            fullDetail: true,
+            country: country
+        })
+        .catch(err => console.error(err))
+        .then(async apps => {
+
+            for (let app of apps) {
+                var d = moment.utc(app.updated).local();
+                if (d.year() <= minYear) {
+                    printAppInfo(app);
+                }
+            }
+        });
+}
+
+function printAppInfo(app) {
+    console.log("Name : " + app.title);
+    console.log("Updated : " + moment.utc(app.updated).local().toDate()) + "\n";
+    console.log("Installs : " + app.installs + "\n");
+}
+
+searchTop100OutdatedApps("Photo", 2020, 'us');
